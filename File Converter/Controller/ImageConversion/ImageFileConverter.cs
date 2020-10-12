77a6dcc8eac621e8c0de8dba5db1b7fed8df7cda
Djebbes Zakaria <orochi255@hotmail.fr>
2020-10-12 06:19:03 +0200

    Webp filesc (Unfinished)

---
+++ File Converter/Controller/ImageConversion/ImageFileConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Imazen.WebP;
+
+namespace File_Converter.Controller.ImageConversion
+{
+	internal abstract class ImageFileConverter : FileConverter
+	{
+		protected string ToImageFormat(string path, ImageFormat format)
+		{
+			string tempPath = GetTempPath();
+			using Image img = Image.FromFile(path);
+			OnFileConverting(path, new Random().Next(15, 69));
+			img.Save(tempPath, format);
+			OnFileConverting(path, 100);
+
+			return tempPath;
+		}
+
+		protected string WebpToFileFormat(string path, ImageFormat format)
+		{
+			string tempPath = GetTempPath();
+
+			using FileStream fileStream = File.OpenRead(path);
+			using MemoryStream stream = new MemoryStream();
+			fileStream.CopyTo(stream);
+
+			SimpleDecoder decoder = new SimpleDecoder();
+			Bitmap result = decoder.DecodeFromBytes(stream.ToArray(), stream.Length);
+			result.Save(tempPath, format);
+
+			return tempPath;
+		}
+	}
+}
